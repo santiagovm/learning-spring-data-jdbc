f3338603a70dbd5e9ac337b75d5f826474eed4ea
Santiago Vasquez <svasquez@pivotal.io>
2020-04-06 16:42:59 -0400

    create project using spring initializer

---
+++ src/main/java/pivotal/io/learningspringdatajdbc/LearningSpringDataJdbcApplication.java
@@ -0,0 +1,13 @@
+package pivotal.io.learningspringdatajdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringDataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringDataJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pivotal/io/learningspringdatajdbc/LearningSpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package pivotal.io.learningspringdatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
