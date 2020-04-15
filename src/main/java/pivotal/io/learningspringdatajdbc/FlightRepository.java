f0e5291d9a16ca6b219fa1c0e024ea4ec61a2a35
Santiago Vasquez <svasquez@pivotal.io>
2020-04-15 08:59:34 -0400

    fix long lines

--- src/main/java/pivotal/io/learningspringdatajdbc/FlightRepository.java
+++ src/main/java/pivotal/io/learningspringdatajdbc/FlightRepository.java
@@ -9,12 +9,19 @@ import java.util.List;
 interface FlightRepository extends CrudRepository<Flight, Long>, DeleteByOriginRepository {
 
     // Spring Data JDBC does not support derived queries yet. Query must be added using @Query annotation
-    @Query("select id, origin, destination, scheduled_at, created_by, created_date, last_modified_by, last_modified_date from flight where origin = :origin")
+    @Query("select id, origin, destination, scheduled_at, created_by, created_date, last_modified_by, last_modified_date "
+            + "from flight "
+            + "where origin = :origin")
     List<Flight> findByOrigin(@Param("origin") String origin);
 
-    @Query("select id, origin, destination, scheduled_at, created_by, created_date, last_modified_by, last_modified_date from flight order by origin")
+    @Query("select id, origin, destination, scheduled_at, created_by, created_date, last_modified_by, last_modified_date "
+            + "from flight "
+            + "order by origin")
     List<Flight> findAllSortedByOrigin();
 
-    @Query("select id, origin, destination, scheduled_at, created_by, created_date, last_modified_by, last_modified_date from flight order by id limit :limit offset :offset")
+    @Query("select id, origin, destination, scheduled_at, created_by, created_date, last_modified_by, last_modified_date "
+            + "from flight "
+            + "order by id "
+            + "limit :limit offset :offset")
     List<Flight> findAllPaginated(@Param("offset") int offset, @Param("limit") int limit);
 }
